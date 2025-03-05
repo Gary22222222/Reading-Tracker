fe496b8475c1fa7091b882a0277a09dfe7f8090e
slooonya <mamonovasofia@gmail.com>
2025-03-05 16:43:30 +0800

    New Spring Boot project files created

---
+++ src/test/java/com/group20/dailyreadingtracker/DailyreadingtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group20.dailyreadingtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyreadingtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
