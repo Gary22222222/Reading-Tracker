e00f57428d702231674723060e1234ad405022eb
Jianghui.Sun <1353761932@qq.com>
2025-03-19 18:16:47 +0800

    First commit

---
+++ src/main/java/com/group20/dailyreadingtracker/entity/ReadingLog.java
@@ -0,0 +1,38 @@
+package com.group20.dailyreadingtracker.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reading_logs")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReadingLog {
+    @Id
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;  // 关联用户
+
+    private String title;   // 书籍或文章标题
+    private String author;  // 作者
+    private LocalDate date; // 阅读日期
+    private int timeSpent;  // 阅读时间（单位：分钟）
+    private String notes;   // 个人笔记
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+}
+
---
+++ src/main/java/com/group20/dailyreadingtracker/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.group20.dailyreadingtracker.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // 根据 email 查找用户（用于登录和注册验证）
+    Optional<User> findByEmail(String email);
+
+    // 检查某个 email 是否已存在
+    boolean existsByEmail(String email);
+}
+
