9f6cd6659e547c5d37327c33efb380b337d73b21
Sonya <94686155+slooonya@users.noreply.github.com>
2025-03-21 12:02:21 +0800

    Revert "First commit"
    This reverts commit dc4d884e3dff6f948b7b45c395a9ec3dcf4e0771.

--- src/main/java/com/group20/dailyreadingtracker/repository/UserRepository.java
+++ src/main/java/com/group20/dailyreadingtracker/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.group20.dailyreadingtracker.repository;
 
 
-import com.group20.dailyreadingtracker.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
--- src/main/java/com/group20/dailyreadingtracker/role/RoleRepository.java
+++ src/main/java/com/group20/dailyreadingtracker/role/RoleRepository.java
@@ -1,6 +1,5 @@
-package com.group20.dailyreadingtracker.repository;
+package com.group20.dailyreadingtracker.role;
 
-import com.group20.dailyreadingtracker.role.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long>{
